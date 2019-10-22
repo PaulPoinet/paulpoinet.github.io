10fb820cfca168827177825332686b1233d65398
Poinet Paul <poinetp@gmail.com>
2019-10-22 15:41:27 +0100

    deployment

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,14 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'www.paulpoinet.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:paulpoinet/paulpoinet.github.io.git master
+git push -f git@github.com:PaulPoinet/paulpoinet.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
