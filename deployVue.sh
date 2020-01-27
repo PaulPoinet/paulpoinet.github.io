2090634862248cb9ebbb4b0ba7512590668f82d0
paulpoinet <poinetp@gmail.com>
2020-01-27 14:36:39 +0000

    deploy

--- deployVue.sh
+++ deployVue.sh
@@ -11,7 +11,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:paulpoinet/paulpoinet.github.io.git gh-pages
-
+git push -f git@github.com:paulpoinet/VueGithubPages.git master
 cd -
 
